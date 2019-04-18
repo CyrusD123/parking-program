3d41efcc000bbad8faaa98b06d98c388fec2e677
CyrusD123 <pseudonym112304@gmai.com>
2019-04-18 17:55:10 -0400

    Condenses PHP

--- web/index.php
+++ web/index.php
@@ -1,1 +1,25 @@
-<?php include_once("index.html"); ?>
+<html>
+<title>Parking Spaces</title>
+<script src = "parking.js"></script>
+<body>
+    <h1>Empty Spaces:</h1>
+    <h2 id="emptyList"></h2>
+    <script>
+        listEmpty();
+    </script>
+    <button type="button" onclick=listEmpty();>Update Empty Spaces</button>
+    <form>
+        <br>
+        Leave a Space: <br>
+        <input type="number" id="leave_num" min="1" max="20" value="1">
+        <input type="button" onclick=emptySpace(); value="Submit">
+    </form>
+    <br>
+    <form>
+        Occupy an Empty Space: <br>
+        <input type="number" id="occupy_num" min="1" max="20" value="1">
+        <input type="button" onclick=occupySpace(); value="Submit">
+    </form>
+
+</body>
+</html>
