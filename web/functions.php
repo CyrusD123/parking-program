5b059dea8f6aa752751bc552f5e4077eb04f0885
CyrusD123 <pseudonym112304@gmai.com>
2019-04-29 15:53:43 -0400

    Debugging

--- web/functions.php
+++ web/functions.php
@@ -8,7 +8,10 @@ $db = substr($url["path"], 1);
 // Create connection
 $conn = new mysqli($server, $username, $password, $db);
 
-echo $server, $username, $password, $db;
+echo $server
+echo $username;
+echo $password;
+echo $db;
 function listEmpty() {
     $result = $conn->query("SELECT * FROM lots");
 
