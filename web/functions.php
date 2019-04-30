f245f138e148c999d0e5c0816a620444aa5f5873
CyrusD123 <pseudonym112304@gmai.com>
2019-04-29 21:22:17 -0400

    Debugging

--- web/functions.php
+++ web/functions.php
@@ -6,7 +6,7 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-echo $password;
+echo $db;
 // Create connection
 $conn = new mysqli($server, $username, $password, $db);
 
