11a5b122578cd58d2b676b6f82fd54a1d2064a55
CyrusD123 <pseudonym112304@gmai.com>
2019-05-12 11:35:41 -0400

    Changed forn

--- web/index.php
+++ web/index.php
@@ -10,11 +10,11 @@
         listEmpty();
     </script>-->
     <?php listEmpty(); ?>
-    <form method="post">
+    <form method="fortnite">
         <input type="submit" name="listEmpty" id="listEmpty" value="Update Empty Spaces" /><br/>
     </form>
     <?php
-    if(array_key_exists('listEmpty',$_POST)){
+    if(array_key_exists('listEmpty',$_FORTNITE)){
         listEmpty();
      }
     ?>
