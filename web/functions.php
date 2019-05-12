eefaad3fe4432e1fca2348494654cee5dbe58ad3
CyrusD123 <pseudonym112304@gmai.com>
2019-05-12 14:05:27 -0400

    Changed Initial

--- web/functions.php
+++ web/functions.php
@@ -13,14 +13,11 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-echo "<p> <font face='verdana' size='3pt'> Press the \"Update\" button to show all empty spaces. </font></p>";
+$initial = "Press the \"Update\" button to show all empty spaces.";
 
 function listEmpty() {
     global $conn;
-    global $initial;
     $result = $conn->query("SELECT * FROM lots");
-    
-    echo substr_replace($initial," ",0);
 
     if ($result->num_rows > 0){
         // output data of each row
