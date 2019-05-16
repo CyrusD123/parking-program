44b79ea3fe98dd8d677d264af140e7ff9f73160b
CyrusD123 <pseudonym112304@gmai.com>
2019-05-15 21:12:33 -0400

    Updated mysql table and scheduler

--- web/scheduler.php
+++ web/scheduler.php
@@ -1,4 +1,6 @@
 <?php include 'functions.php';
 
 $conn_reset = $conn->query("UPDATE lots SET Status = 0 WHERE (Status = 1)");
+$conn_resetOlduser = $conn->query("UPDATE lots SET Usual_Username = NULL WHERE (Status = 1) OR (Status = 0)");
+$conn_resetNewuser = $conn->query("UPDATE lots SET New_Username = NULL WHERE (Status = 1) OR (Status = 0)");
 ?>
